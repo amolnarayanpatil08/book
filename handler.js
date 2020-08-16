b38326418aa5dc1f58996c1e8989abf28bee0af8
Amol Patil <amolnarayanpatil08@gmail.com>
2020-08-16 19:11:15 +0530

    done with get api

--- handler.js
+++ handler.js
@@ -69,15 +69,125 @@ function Available(bookn){
     })
 }
 
+  function getByBookId(id) {
+    var params = {
+        TableName: "books_dev",
+        Key: {
+            book_id:parseInt(id)
+        }
+    };
+
+    return new Promise((res,rej)=>{
+        docClient.get(params, (err,response)=>{  
+            if(err){
+                rej(err);
+            }
+            if(response){
+               res(response);
+            }
+        });
+    })
+}
+
+
+ function  getByBookName(name) {
+    var params = {
+        TableName: "books_dev",
+        IndexName: "book_name-index",
+        KeyConditionExpression: "book_name = :book_name",
+    ExpressionAttributeValues: {
+        ":book_name": name
+    },
+    };
+    return new Promise((res,rej)=>{
+        docClient.query(params, (err,response)=>{  
+            if(err){
+                rej(err);
+            }
+            if(response){
+               res(response);
+            }
+        });
+    })
+}
+
+  function getByAuthor(AuthorName) {
+    var params = {
+        TableName: "books_dev",
+        IndexName: "author-index",
+        KeyConditionExpression: "author = :author_name",
+    ExpressionAttributeValues: {
+        ":author_name": AuthorName
+    },
+    };
+    return new Promise((res,rej)=>{
+        docClient.query(params, (err,response)=>{  
+            if(err){
+                rej(err);
+            }
+            if(response){
+
+		let arr=[];
+		for(i=0;i<response.Items.length;i++)
+		arr.push(response.Items[i].book_name);
+		res((arr));
+              // res(response);
+            }
+        });
+    })
+}
+
 async function isAvailable(book_name) {
     const response = await Available(book_name);
     return response;
 }
 
+
+ async function byId(ctx) {
+
+	console.log(ctx.url);
+      await getByBookId(ctx.params.Id).then(
+        result => {
+            ctx.body=result;
+        }
+    ).catch(err=>{
+        ctx.body=err;
+    }
+     );
+}
+ async function byName(ctx) {
+      await getByBookName(ctx.params.Name).then(
+        result => {
+            ctx.body=result;
+        }
+    ).catch(err=>{
+        ctx.body=err;
+    }
+     );
+}
+ async function byAuthor(ctx) {
+      await getByAuthor(ctx.params.Author).then(
+        result => {
+            ctx.body=result;
+        }
+    ).catch(err=>{
+        ctx.body=err;
+    }
+     );
+}
+
+
 module.exports = {
     getIdByName,
     getId,
     isAvailable,
-    Available
+    Available,
+    getByBookId,
+	getByBookName,
+	getByAuthor,
+	byId,
+	byName,
+	byAuthor,
+    
 }
 
